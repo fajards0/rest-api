5d963f2825f69747a489d0a7771f93e6670961d9
fajards0 <fajardwisaputro744@gmail.com>
2024-07-30 14:59:58 +0700

    apiharike2

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,32 @@
 <?php
 
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\KategoriController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('kategori', [KategoriController::class, 'index']);
-Route::post('kategori', [KategoriController::class, 'store']);
-Route::get('kategori/{id}', [KategoriController::class, 'show']);
+// KATEGORI
+// Route::get('kategori', [KategoriController::class, 'index']);
+// Route::post('kategori', [KategoriController::class, 'store']);
+// Route::get('kategori/{id}', [KategoriController::class, 'show']);
+// Route::put('kategori/{id}', [KategoriController::class, 'update']);
+// Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
+
+// Route::get('tag', [TagController::class, 'index']);
+// Route::post('tag', [TagController::class, 'store']);
+// Route::get('tag/{id}', [TagController::class, 'show']);
+// Route::put('tag/{id}', [TagController::class, 'update']);
+// Route::delete('tag/{id}', [TagController::class, 'destroy']);
+
+// Route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
+// Route::resource('tag', tagController::class)->except(['edit', 'create']);
+
+
+Route::apiResource('kategori', KategoriController::class);
+Route::apiResource('tag', TagController::class);
+Route::apiResource('user', UserController::class);
