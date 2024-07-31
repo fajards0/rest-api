522b0bfd979653c49ba873551da378b5aeed43bc
fajards0 <fajardwisaputro744@gmail.com>
2024-07-31 17:04:16 +0700

    apihari3

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Berita extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'id_kategori');
+    }
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class, 'tag_berita', 'id_berita', 'id_tag');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BeritaController;
 use App\Http\Controllers\Api\KategoriController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user/profile', function (Request $request) {
     return $request->user();
 });
 
@@ -26,7 +28,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
 // Route::resource('tag', tagController::class)->except(['edit', 'create']);
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('kategori', KategoriController::class);
-Route::apiResource('tag', TagController::class);
-Route::apiResource('user', UserController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('kategori', KategoriController::class);
+    Route::apiResource('tag', TagController::class);
+    Route::apiResource('user', UserController::class);
+    Route::apiResource('berita', BeritaController::class);
+});
